4249c790c91b97a901a87f631274fbf40f60aa46
Matipa Marufu <2344503@students.wits.ac.za>
2022-05-31 09:48:32 +0200

    Responsive design not working

--- JS/carousel.js
+++ JS/carousel.js
@@ -1,6 +1,6 @@
 let slideIndex = [1,1];
 /* Class the members of each slideshow group with different CSS classes */
-let slideId = ["mySlides1", "mySlides2"]
+let slideId = ["slides-1", "slides-2"]
 showSlides(1, 0);
 showSlides(1, 1);
 
