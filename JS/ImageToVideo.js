c6bcc08da55ecb50c02d08d542f7ca619d269f33
Matipa Marufu <2344503@students.wits.ac.za>
2022-06-22 14:33:05 +0200

    post assignment 2 changes

---
+++ JS/ImageToVideo.js
@@ -0,0 +1,50 @@
+let codeImage = document.getElementById("CodeImage");
+let codeVideo = document.getElementById("CodeVideo");
+
+let codeImage2 = document.getElementById("CodeImage2");
+let codeVideo2 = document.getElementById("CodeVideo2");
+
+let codeImage3 = document.getElementById("CodeImage3");
+let codeVideo3 = document.getElementById("CodeVideo3");
+
+function changeToVideo()
+{
+    codeImage.classList.add("hide");
+    codeVideo.classList.remove("hide")
+
+    // setTimeout(() => {
+    //     console.log("Waited for 5 seconds")
+    // }, "5000")
+    
+    // codeImage.classList.remove("hide");
+    // codeVideo.classList.add("hide")
+
+}
+
+function changeToVideo2()
+{
+    codeImage2.classList.add("hide");
+    codeVideo2.classList.remove("hide")
+
+    // setTimeout(() => {
+    //     console.log("Waited for 5 seconds")
+    // }, "5000")
+    
+    // codeImage.classList.remove("hide");
+    // codeVideo.classList.add("hide")
+
+}
+
+function changeToVideo3()
+{
+    codeImage3.classList.add("hide");
+    codeVideo3.classList.remove("hide")
+
+    // setTimeout(() => {
+    //     console.log("Waited for 5 seconds")
+    // }, "5000")
+    
+    // codeImage.classList.remove("hide");
+    // codeVideo.classList.add("hide")
+
+}
