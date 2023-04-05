3f2dd727838753fa43e83e87a57328e4200c2cf2
Matipa Marufu <2344503@students.wits.ac.za>
2023-04-05 17:33:48 +0200

    updated nav bar for every page

--- JS/Navigation.js
+++ JS/Navigation.js
@@ -3,65 +3,21 @@ function BakeryGame()
     location.href = "BakeryGame.html";
 }
 
-function BlogPosts()
-{
-    location.href = "blogPosts.html";
-}
 
-function ContactMe()
-{
-    location.href = "contactMe.html"
-}
+// function ContactMe()
+// {
+//     location.href = "contactMe.html"
+// }
 
 function ContactThanks()
 {
     location.href = "contactThanks.html"
 }
 
-function CreativeReflection()
-{
-    location.href = "CreativeReflection.html"
-}
-
-function CriticalReflection()
-{
-    location.href = "CriticalReflection.html"
-}
-
-function CriticalWriting2()
-{
-    location.href = "CriticalWriting2.html"
-}
-
-function CriticalWriting4()
-{
-    location.href = "CriticalWriting4.html"
-}
-
-function CriticalWriting6()
-{
-    location.href = "CriticalWriting6.html"
-}
-
-function CriticalWriting9()
-{
-    location.href = "CriticalWriting9.html"
-}
-
-function CriticalWriting11()
-{
-    location.href = "CriticalWriting11.html"
-}
-
-function CriticalWriting14()
-{
-    location.href = "CriticalWriting14.html"
-}
-
-function Development()
-{
-    location.href = "Development.html"
-}
+// function Development()
+// {
+//     location.href = "Development.html"
+// }
 
 function ExternalEssay()
 {
@@ -79,12 +35,12 @@ function Firtech()
     location.href = "Firtech.html";
 }
 
-function Index()
-{
-    console.log("button pressed");
-    location.href = "index.html";
+// function Index()
+// {
+//     console.log("button pressed");
+//     location.href = "index.html";
  
-}
+// }
 
 function PlantsVsInsects()
 {
@@ -96,20 +52,94 @@ function Profile()
     location.href = "Profile.html";
 }
 
-function Reflections()
-{
-    location.href = "reflections.html";
-}
-
-function TechnicalReflection()
-{
-    location.href = "TechnicalReflection.html";
-}
-
 function WomansFlow()
 {
     location.href = "WomansFlow.html";
 }
+
+
+//NAV BAR
+const navMenu = ["index.html","Development.html","contactMe.html", "Profile.html"];
+// let navArea = document.getElementById('nav');
+let header = document.getElementById("header");
+header.classList.add("h-card");
+
+let footer = document.getElementById("footer");
+footer.classList.add("h-card");
+
+footer.innerText = "ocmatipa@gmail.com";
+
+let  nav = document.createElement("nav");
+header.append(nav);
+nav.classList.add("h-entry");
+nav.classList.add("#nav");
+
+let ul = document.createElement("ul");
+nav.append(ul);
+
+console.log("file is working");
+
+// let navButtons = header.getElementsByTagName('a');
+// let icons = navArea.getElementsByTagName('i');
+
+for(let i = 0; i < navMenu.length; i++)
+{
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    let iLink =  document.createElement("i");
+
+    ul.append(li);
+    li.append(a);
+    a.append(iLink);
+
+    a.classList.add("u-url");
+    iLink.classList.add("fa-solid");
+
+    let className;
+    let className2;
+    let innerText;
+
+    switch(i)
+    {
+        case 0:
+            className = "fa-house-chimney";
+            innerText = "Home";
+            break;
+        case 1:
+            className = "fa-solid";
+            className2 = "fa-code";
+            innerText = "Development";
+
+            break;
+        case 2:
+            className ="fa-solid";
+            className2 = "fa-address-book";
+            innerText = "Contact Me";
+            break;
+        case 3:
+            className = "fa-solid";
+            className2 = "fa-id-card-clip";
+            innerText = "About Me";
+            break;
+    }
+
+    iLink.classList.add(className);
+
+    if(className2 != null)
+    {
+        iLink.classList.add(className2);
+
+    }
+
+    iLink.innerHTML = innerText;
+
+    a.onclick = function()
+    {
+        location.href = navMenu[i]
+    }
+}
+
+window.onresize = function(){ location.reload(); }
 
 
 
